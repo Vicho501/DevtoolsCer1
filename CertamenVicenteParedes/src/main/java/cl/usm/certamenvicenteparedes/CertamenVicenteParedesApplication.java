d26f870c111734701fc628442f84f9362319e278
vicente <vicente.paredesp@usm>
2026-04-24 01:54:14 -0400

    base del codigo para el certamen

---
+++ CertamenVicenteParedes/src/main/java/cl/usm/certamenvicenteparedes/CertamenVicenteParedesApplication.java
@@ -0,0 +1,13 @@
+package cl.usm.certamenvicenteparedes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertamenVicenteParedesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertamenVicenteParedesApplication.class, args);
+    }
+
+}
---
+++ CertamenVicenteParedes/src/test/java/cl/usm/certamenvicenteparedes/CertamenVicenteParedesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usm.certamenvicenteparedes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertamenVicenteParedesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
